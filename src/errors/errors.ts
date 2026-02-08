d124bf2e5dca1b0069e9a081aac9be5bd3df56ab
merger3 <glenn1999@verizon.net>
2026-02-08 00:04:48 -0500

    formatting

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,5 +1,3 @@
-export const ErrCameraNotSet = new Error("Camera not set on context");
-export const ErrUnableToCallVAPIX = (cause: any) =>
-	new Error("Unable to make VAPIX call", { cause: cause });
-export const ErrVAPIXCallFailed = (cause: any) =>
-	new Error("VAPIX call failed", { cause: cause });
+export const ErrCameraNotSet      = new Error("Camera not set on context");
+export const ErrUnableToCallVAPIX = (cause: any) => new Error("Unable to make VAPIX call", { cause: cause });
+export const ErrVAPIXCallFailed   = (cause: any) => new Error("VAPIX call failed", { cause: cause });
